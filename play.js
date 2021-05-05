fbd0631a840c846a6ff5f2010146c3cd924e4518
archaemedes <wadecroft@gmail.com>
2021-05-05 22:30:34 +0000

    Wrote handleuserinput function and stdin

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
 const {connect} = require("./client")
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (input) {
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
 
 console.log("Connecting ...");
+setupInput();
+stdin.on("data", handleUserInput);
 connect();
